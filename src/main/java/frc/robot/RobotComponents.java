9f0e4e647c5ff518f8eb6b28fcbe41e258d1c2a3
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-09-26 20:39:26 +0300

    addede RobotMap

--- src/main/java/frc/robot/RobotComponents.java
+++ src/main/java/frc/robot/RobotComponents.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 /** This class sets what all of the robot instance components are */
 public class RobotComponents {
+    
     /* Add your class components here, and remove comment. example: 
 
     public static class Drivetrain {
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,26 +1,17 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
+/** This class defines the ports of the robot */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+    
+    // Device ID's for devices connected to the CAN bus
+    static class CAN {
+    }
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+    // Solenoid ports connected to PCM
+    static class PCM {
+    }
+
+    // Robot components connected to roboRIO Digital Input Output ports
+    static class DIO {
+    }
 }
