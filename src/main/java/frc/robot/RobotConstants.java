26e220d7e7d869a3b605fd8fe736f34c2fdb9ac3
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-09-26 19:57:08 +0300

    added RobotConstanse

---
+++ src/main/java/frc/robot/RobotConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** This class used to store constants related to the robot */
+public class RobotConstants {
+    
+    /** Physical measurements of the robot */
+    public static class RobotDimensions {
+    }
+
+    /** All the robot pid values for monitoring */
+    public static class RobotPIDSettings {
+    }
+    
+    /** Constants for sensors on the robot */
+    public static class Sensors {
+    }
+}
