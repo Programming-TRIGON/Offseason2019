0ce7d12c11975e39d8eaa2693448db7c37f991e4
moshechelsky <MosheChelsky@gmail.com>
2019-09-27 11:32:00 +0300

    added sparkmax library

---
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -0,0 +1,12 @@
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/** This is the susbsystem for the drivetrain of the robot */
+public class Drivetrain extends Subsystem {
+  
+
+  @Override
+  public void initDefaultCommand() {
+  }
+}
