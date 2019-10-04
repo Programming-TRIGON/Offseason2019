9aecb86a5c06751b20cc1e57345a548b920bf617
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-10-04 14:51:05 +0300

    apdate set piston func

--- src/main/java/frc/robot/Subsystems/HatchHolder.java
+++ src/main/java/frc/robot/Subsystems/HatchHolder.java
@@ -8,42 +8,34 @@ import frc.robot.RobotComponents;
 /** Hatch holder Subsystem catchs the hatch and eject it */
 public class HatchHolder extends Subsystem {
   private DoubleSolenoid lockerSolenoid, ejectorSolenoid;
-  private Value lockState = Value.kForward, ejectionState = Value.kReverse;
-  private boolean lockStateB = true, ejectionStateB = false;
+  private boolean lockState = true, ejectionState = false;
 
   public HatchHolder() {
     this.lockerSolenoid = RobotComponents.HatchHolder.LOCK_SOLENOID;
     this.ejectorSolenoid = RobotComponents.HatchHolder.EJECT_SOLENOID;
     
     // default values
-    setLock(Value.kReverse);
-    setEjection(Value.kReverse);
+    setLock(false);
+    setEjection(false);
   }
 
   /**
    * sets the status of the front solenoid, the one that chatches the Hatch.
    */
-  public void setLock(Value value) {
-    if (ejectionState == Value.kReverse) {
-      lockState = value;
-      this.lockerSolenoid.set(value);
-    }
-  }
-
   public void setLock(boolean lock) {
-    if (ejectionStateB == false) {
-      lockStateB = lock;
-      this.lockerSolenoid.set(Value.kForward);
+    if (ejectionState == false) {
+      lockState = lock;
+      this.lockerSolenoid.set(booleanToValue(lock));
     }
   }
 
   /**
    * sets the status of the rear solenoids, the ones that pushes the subsystem outwards.
    */
-  public void setEjection(Value value) {
-    if(lockState == Value.kReverse){
-      ejectionState = value;      
-      this.ejectorSolenoid.set(value);
+  public void setEjection(boolean eject) {
+    if(!lockState){
+      ejectionState = eject;      
+      this.ejectorSolenoid.set(booleanToValue(eject));
     }
   }
 
@@ -52,8 +44,10 @@ public class HatchHolder extends Subsystem {
     this.ejectorSolenoid.set(Value.kOff);
   }
 
-  private Value booleanToValue(boolean bool){
-    return bool ? Value.kForward : Value.kReverse;
+  /** Convert boolean to DoubleSolenoid Value,
+   *  true pushes the piston forward and false reverse the piston */
+  private Value booleanToValue(boolean forward){
+    return forward ? Value.kForward : Value.kReverse;
   }
 
   @Override
