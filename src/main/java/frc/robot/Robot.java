8163590247eab53cec1be930a333231833e3679c
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-10-06 15:11:04 +0300

    subsystem folder add import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.HatchHolder;
 import frc.robot.subsystems.Lift;
 
 public class Robot extends TimedRobot {
