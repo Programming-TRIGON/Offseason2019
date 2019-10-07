f425a50d968e754cd0190807ccbfbb0fe00b18e8
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-10-07 22:31:06 +0300

    more changes

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,22 +1,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  
-  public XboxController driver = new XboxController(0);
-  public Button MaxSpeedStop, DriveStraightWithGyro; 
+  public XboxController driver;
 
   public OI(){
-
-    MaxSpeedStop = new JoystickButton(driver, 3);
-    DriveStraightWithGyro = new JoystickButton(driver, 2);
+    this.driver = new XboxController(0);
   }
-
 }
