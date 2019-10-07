fd9b940a7a4654d086fe185666f014d433d990e9
Mboesn <>
2019-10-07 22:08:43 +0300

    more changes

--- src/main/java/frc/robot/Enums.java
+++ src/main/java/frc/robot/Enums.java
@@ -9,26 +9,26 @@ import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Waypoint;
 
 /**
- * This class contains enums for the robot,
- * we convert human readable language to the robot language 
+ * This class contains enums for the robot, we convert human readable language
+ * to the robot language
  */
 public class Enums {
     public enum Path {
-        ROCKET(new Waypoint[] { new Waypoint(0, 0, 0), new Waypoint(2, -3, 0) }),
-        SHIP("ship.csv"),
-        PATH2("path2.csv"),
-        PATH3("path3.csv"),
-        TEST_JACI(new Waypoint[] { new Waypoint(0, 0, 0), new Waypoint(1, -1, -90) });
-    
+        SHIP("ship.csv"), PATH2("path2.csv"),
+        PATH2_JACI(new Waypoint[] { new Waypoint(0, 0, 0),
+                new Waypoint(0, RobotConstants.FieldDimensions.PATH2_DISTANCE, 0) }),
+        PATH3("path3.csv"), PATH3_JACI(new Waypoint[] { new Waypoint(0, 0, 0),
+                new Waypoint(0, RobotConstants.FieldDimensions.PATH3_DISTANCE, 0) });
+
         private final Trajectory trajectory;
-    
+
         private Path(Waypoint[] path) {
             trajectory = Pathfinder.generate(path,
                     new Trajectory.Config(Trajectory.FitMethod.HERMITE_QUINTIC, Trajectory.Config.SAMPLES_HIGH,
                             RobotConstants.MotionProfiling.TIMEFRAME, RobotConstants.MotionProfiling.MAX_VELOCITY,
                             RobotConstants.MotionProfiling.MAX_ACCELERATION, RobotConstants.MotionProfiling.MAX_JERK));
         }
-    
+
         private Path(File csvFile) {
             Trajectory trajectory = null;
             try {
@@ -37,9 +37,9 @@ public class Enums {
                 System.err.println("File not existing");
             }
             this.trajectory = trajectory;
-    
+
         }
-    
+
         /**
          * loads path from a PathPlanner formatted csv file.
          *
@@ -48,10 +48,9 @@ public class Enums {
         Path(String pathPlannerFile) {
             trajectory = CsvReader.read("/home/lvuser/paths/" + pathPlannerFile);
         }
-    
+
         public Trajectory getTrajectory() {
             return trajectory;
         }
     }
-    
 }
