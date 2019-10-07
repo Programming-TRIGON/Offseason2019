e0a3b23035ccb1acb99184ebb1addb2c302451b8
Mboesn <>
2019-10-08 01:36:11 +0300

    finished

---
+++ src/main/java/frc/robot/command_group/CollectHatchFromFeeder.java
@@ -0,0 +1,21 @@
+
+package frc.robot.command_group;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Enums.LiftHeights;
+import frc.robot.commands.HatchHolderLock;
+import frc.robot.commands.setLiftHeight;
+/**
+ * this command collects hatch from the feeder.
+ */
+public class CollectHatchFromFeeder extends CommandGroup {
+
+  public CollectHatchFromFeeder() {
+    addSequential(new setLiftHeight(LiftHeights.Feeder));
+    addSequential(new HatchHolderLock(false));
+    addSequential(new HatchHolderEject(true));
+    addSequential(new HatchHolderLock(true));
+    addSequential(new HatchHolderEject(false));
+    
+  }
+}
