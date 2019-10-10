da5f4cfadc8af87d9b28cb0fef59f71d9369cf67
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2019-10-10 23:37:29 +0300

    added wait until in score cargo

--- src/main/java/frc/robot/command_group/ScoreCargo.java
+++ src/main/java/frc/robot/command_group/ScoreCargo.java
@@ -1,6 +1,9 @@
 package frc.robot.command_group;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import frc.robot.Robot;
+import frc.robot.commands.WaitUntil;
 import frc.robot.commands.setLiftHeight;
 import frc.robot.RobotConstants;
 import frc.robot.Enums.LiftHeights;
@@ -8,14 +11,14 @@ import frc.robot.Enums.Target;
 import frc.robot.commands.EjectCargo;
 import frc.robot.commands.VisionPID;
 
-
 public class ScoreCargo extends CommandGroup {
 
   /** Scores the cargo. */
   public ScoreCargo(LiftHeights height) {
     Target target = RobotConstants.liftVisionMap.get(height);
     addParallel(new VisionPID(target, true));
-    addSequential(new setLiftHeight(height));
+    addParallel(new setLiftHeight(height));
+    addSequential(new WaitUntil(Robot.lift::isOnTarget));
     addSequential(new EjectCargo());
   }
 }
