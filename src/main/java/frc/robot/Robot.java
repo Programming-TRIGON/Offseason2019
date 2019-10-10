d0c516ab13ef58053116cb54585c8f7896ec62c2
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2019-10-10 22:07:53 +0300

    added testing to Robot class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,11 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.SetLiftHeight;
+import frc.robot.commands.TestPID;
+import frc.robot.commands.VisionPID;
+import frc.robot.motionprofiling.FollowPath;
+import frc.robot.motionprofiling.Path;
 import frc.robot.motionprofiling.PathCreater;
 import frc.robot.subsystems.CargoHolder;
 import frc.robot.subsystems.Drivetrain;
@@ -40,10 +45,18 @@ public class Robot extends TimedRobot {
     lift = new Lift();
     hatchHolder = new HatchHolder();
 
-    // autonomousChooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // autonomousChooser.addOption("Auto", new AutoCommand());
-    SmartDashboard.putData("Auto mode", autonomousChooser);
-  }
+        // autonomousChooser.setDefaultOption("Default Auto", new ExampleCommand());
+        // autonomousChooser.addOption("Auto", new AutoCommand());
+        SmartDashboard.putData("Auto mode", autonomousChooser);
+        SetLiftHeight liftCommand = new SetLiftHeight(Enums.LiftHeights.RocketHatchMiddle);
+        SmartDashboard.putData("Test PID lift", new TestPID(liftCommand, liftCommand::setPID));
+        VisionPID visionCommand = new VisionPID(Enums.Target.RocketMiddle, true);
+        SmartDashboard.putData("Test PID vision", new TestPID(visionCommand, visionCommand::setPID,
+                visionCommand::setPID2));
+        FollowPath motionProfileCommand = new FollowPath(Path.TEST);
+        SmartDashboard.putData("Test PIDVA motion profile", new TestPID(motionProfileCommand,
+                motionProfileCommand::setPID, motionProfileCommand::setPID2));
+    }
 
   @Override
   public void robotPeriodic() {
