b76886d3b5f04874fc2cc3502e5c03a0dd31c9e8
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2019-10-10 22:07:53 +0300

    added testing to Robot class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,11 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.SetLiftHeight;
+import frc.robot.commands.TestPID;
+import frc.robot.commands.VisionPID;
+import frc.robot.motionprofiling.FollowPath;
+import frc.robot.motionprofiling.Path;
 import frc.robot.motionprofiling.PathCreater;
 import frc.robot.subsystems.CargoHolder;
 import frc.robot.subsystems.Drivetrain;
@@ -13,74 +18,82 @@ import frc.robot.subsystems.Lift;
 import frc.robot.utils.Limelight;
 
 public class Robot extends TimedRobot {
-  public static OI oi;
-  public static PathCreater pathCreater;
-  public static CargoHolder cargoHolder;
-  public static Drivetrain drivetrain;
-  public static Lift lift;
-  public static HatchHolder hatchHolder;
-  public static Limelight limelight;
-  private Command autonomousCommand;
-  private SendableChooser<Command> autonomousChooser = new SendableChooser<>();
+    public static OI oi;
+    public static PathCreater pathCreater;
+    public static CargoHolder cargoHolder;
+    public static Drivetrain drivetrain;
+    public static Lift lift;
+    public static HatchHolder hatchHolder;
+    public static Limelight limelight;
+    private Command autonomousCommand;
+    private SendableChooser<Command> autonomousChooser = new SendableChooser<>();
 
-  @Override
-  public void robotInit() {
-    // Utils:
-    oi = new OI();
-    pathCreater = new PathCreater();
-    limelight = new Limelight();
-    // Subsystems:
-    cargoHolder = new CargoHolder();
-    drivetrain = new Drivetrain();
-    lift = new Lift();
-    hatchHolder = new HatchHolder();
+    @Override
+    public void robotInit() {
+        // Utils:
+        oi = new OI();
+        pathCreater = new PathCreater();
+        limelight = new Limelight();
+        // Subsystems:
+        cargoHolder = new CargoHolder();
+        drivetrain = new Drivetrain();
+        lift = new Lift();
+        hatchHolder = new HatchHolder();
 
-    // autonomousChooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // autonomousChooser.addOption("Auto", new AutoCommand());
-    SmartDashboard.putData("Auto mode", autonomousChooser);
-  }
-
-  @Override
-  public void robotPeriodic() {
-    if (lift.getBottomSwitch())
-      lift.resetEncoderHeight();
-  }
-
-  @Override
-  public void disabledInit() {
-  }
-
-  @Override
-  public void disabledPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  @Override
-  public void autonomousInit() {
-    autonomousCommand = autonomousChooser.getSelected();
-    if (autonomousCommand != null) {
-      autonomousCommand.start();
+        // autonomousChooser.setDefaultOption("Default Auto", new ExampleCommand());
+        // autonomousChooser.addOption("Auto", new AutoCommand());
+        SmartDashboard.putData("Auto mode", autonomousChooser);
+        SetLiftHeight liftCommand = new SetLiftHeight(Enums.LiftHeights.RocketHatchMiddle);
+        SmartDashboard.putData("Test PID lift", new TestPID(liftCommand, liftCommand::setPID));
+        VisionPID visionCommand = new VisionPID(Enums.Target.RocketMiddle, true);
+        SmartDashboard.putData("Test PID vision", new TestPID(visionCommand, visionCommand::setPID,
+                visionCommand::setPID2));
+        FollowPath motionProfileCommand = new FollowPath(Path.TEST);
+        SmartDashboard.putData("Test PIDVA motion profile", new TestPID(motionProfileCommand,
+                motionProfileCommand::setPID, motionProfileCommand::setPID2));
     }
-  }
 
-  @Override
-  public void autonomousPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  @Override
-  public void teleopInit() {
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
+    @Override
+    public void robotPeriodic() {
+        if (lift.getBottomSwitch())
+            lift.resetEncoderHeight();
     }
-  }
 
-  @Override
-  public void teleopPeriodic() {
-    Scheduler.getInstance().run();
-  }
+    @Override
+    public void disabledInit() {
+    }
 
-  @Override
-  public void testPeriodic() {
-  }
+    @Override
+    public void disabledPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void autonomousInit() {
+        autonomousCommand = autonomousChooser.getSelected();
+        if (autonomousCommand != null) {
+            autonomousCommand.start();
+        }
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void teleopInit() {
+        if (autonomousCommand != null) {
+            autonomousCommand.cancel();
+        }
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
 }
