1485d9a36d7a2d0bd03db75dcae041bed40af99f
moshechelsky <MosheChelsky@gmail.com>
2019-10-10 16:37:57 +0300

    fixed stupid freaking bug

--- src/main/java/frc/robot/command_group/ScoreCargo.java
+++ src/main/java/frc/robot/command_group/ScoreCargo.java
@@ -2,6 +2,9 @@ package frc.robot.command_group;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.commands.setLiftHeight;
+import frc.robot.Enums.LiftHeights;
+import frc.robot.commands.EjectCargo;
+import frc.robot.commands.Commands;
 
 
 public class ScoreCargo extends CommandGroup {
