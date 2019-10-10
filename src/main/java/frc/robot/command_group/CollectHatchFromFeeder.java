8ec361f95160c5eff66466bc1ee8dae502668943
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2019-10-10 18:42:15 +0300

    added vision

--- src/main/java/frc/robot/command_group/CollectHatchFromFeeder.java
+++ src/main/java/frc/robot/command_group/CollectHatchFromFeeder.java
@@ -2,9 +2,11 @@
 package frc.robot.command_group;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Enums;
 import frc.robot.Enums.LiftHeights;
 import frc.robot.commands.HatchHolderEject;
 import frc.robot.commands.HatchHolderLock;
+import frc.robot.commands.VisionPID;
 import frc.robot.commands.setLiftHeight;
 /**
  * this command collects hatch from the feeder.
@@ -12,6 +14,7 @@ import frc.robot.commands.setLiftHeight;
 public class CollectHatchFromFeeder extends CommandGroup {
 
   public CollectHatchFromFeeder() {
+    addParallel(new VisionPID(Enums.Target.Feeder,true));
     addSequential(new setLiftHeight(LiftHeights.Feeder));
     addSequential(new HatchHolderLock(false));
     addSequential(new HatchHolderEject(true));
