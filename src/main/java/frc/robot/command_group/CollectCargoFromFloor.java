dce8b7aa0fc9b884a38bf00ba938c9996871562b
Mboesn <>
2019-10-10 23:26:50 +0300

    s to big S

--- src/main/java/frc/robot/command_group/CollectCargoFromFloor.java
+++ src/main/java/frc/robot/command_group/CollectCargoFromFloor.java
@@ -3,7 +3,7 @@ package frc.robot.command_group;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Enums.LiftHeights;
 import frc.robot.commands.CollectCargo;
-import frc.robot.commands.setLiftHeight;
+import frc.robot.commands.SetLiftHeight;
 import frc.robot.commands.Commands;
 
 
@@ -13,8 +13,8 @@ public class CollectCargoFromFloor extends CommandGroup {
    */
   public CollectCargoFromFloor() {
     addSequential(Commands.setTiltCommand(true));
-    addSequential(new setLiftHeight(LiftHeights.floor));
+    addSequential(new SetLiftHeight(LiftHeights.floor));
     addSequential(new CollectCargo());
-    addSequential(new setLiftHeight(LiftHeights.RocketCargoBottom));
+    addSequential(new SetLiftHeight(LiftHeights.RocketCargoBottom));
   }
 }
--- src/main/java/frc/robot/command_group/HatchScore.java
+++ src/main/java/frc/robot/command_group/HatchScore.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Enums.LiftHeights;
 import frc.robot.Enums.Target;
 import frc.robot.commands.VisionPID;
-import frc.robot.commands.setLiftHeight;
+import frc.robot.commands.SetLiftHeight;
 
 /**
  * this command group scores the hatch: with vision, changes the lifts height and then scores
@@ -12,7 +12,7 @@ import frc.robot.commands.setLiftHeight;
 public class HatchScore extends CommandGroup {
   public HatchScore(LiftHeights liftHeight, Target target, boolean isDrivingForward) {
     addParallel(new VisionPID(target, isDrivingForward));
-    addSequential(new setLiftHeight(liftHeight));
+    addSequential(new SetLiftHeight(liftHeight));
     addSequential(new EjectHatch());
   }
 }
