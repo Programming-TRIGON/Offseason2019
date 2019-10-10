032c3a54702c667ea2fd9715276fa77ab53dc927
moshechelsky <MosheChelsky@gmail.com>
2019-10-10 16:29:28 +0300

    added more pf dat ting i wuz werkin on

--- src/main/java/frc/robot/command_group/ScoreCargo.java
+++ src/main/java/frc/robot/command_group/ScoreCargo.java
@@ -1,12 +1,15 @@
 package frc.robot.command_group;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.commands.setLiftHeight;
+
 
 public class ScoreCargo extends CommandGroup {
 
   /** Scores the cargo. */
   public ScoreCargo(LiftHeights height) {
-    addSequential(Commands.)
+    addSequential(Commands.setTiltCommand(true));
     addSequential(new setLiftHeight(height));
+    addSequential(new EjectCargo());
   }
 }
