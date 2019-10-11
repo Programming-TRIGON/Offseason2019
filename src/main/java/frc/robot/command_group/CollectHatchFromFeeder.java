0835c183d1ca7c38a9078229bfe2d043bb49c880
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2019-10-11 09:21:10 +0300

    changed collect hatch to wait for vision ending

--- src/main/java/frc/robot/command_group/CollectHatchFromFeeder.java
+++ src/main/java/frc/robot/command_group/CollectHatchFromFeeder.java
@@ -4,7 +4,6 @@ package frc.robot.command_group;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Enums;
 import frc.robot.Enums.LiftHeights;
-import frc.robot.Robot;
 import frc.robot.commands.*;
 
 /**
@@ -12,15 +11,15 @@ import frc.robot.commands.*;
  */
 public class CollectHatchFromFeeder extends CommandGroup {
 
-  public CollectHatchFromFeeder() {
-    addParallel(new VisionPID(Enums.Target.Feeder,true));
-    setLiftHeight lowerLiftCommand = new setLiftHeight(LiftHeights.HatchBottom);
-    addParallel(lowerLiftCommand);
-    addSequential(new WaitUntil(lowerLiftCommand::isOnTarget));
-    addSequential(new HatchHolderLock(false));
-    addSequential(new HatchHolderEject(true));
-    addSequential(new HatchHolderLock(true));
-    addSequential(new HatchHolderEject(false));
-    
-  }
+    public CollectHatchFromFeeder() {
+        setLiftHeight lowerLiftCommand = new setLiftHeight(LiftHeights.HatchBottom);
+        addParallel(lowerLiftCommand);
+        addSequential(new VisionPID(Enums.Target.Feeder, true));
+        addSequential(new WaitUntil(lowerLiftCommand::isOnTarget));
+        addSequential(new HatchHolderLock(false));
+        addSequential(new HatchHolderEject(true));
+        addSequential(new HatchHolderLock(true));
+        addSequential(new HatchHolderEject(false));
+
+    }
 }
