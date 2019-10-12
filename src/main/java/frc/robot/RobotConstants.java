43dabf03666b9771eca5ef550e57411f16c27417
Hillel <Amiel>
2019-10-12 22:12:02 +0300

    oredered RobotConstant

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -5,7 +5,6 @@ public class RobotConstants {
 
     /** Physical measurements of the robot */
     public static class RobotDimensions {
-
     }
 
     // TODO: set configure pid settings
@@ -22,36 +21,36 @@ public class RobotConstants {
         public static final double DRIVETRAIN_ENCODERS_DISTANCE_PER_TICKS = 4096;
 
         public static final double LIFT_ENCODER_OFFSET = 4096;
-        public static final double LIFT_ENCODER_DISTANCE_PER_TICKS = 4096; 
+        public static final double LIFT_ENCODER_DISTANCE_PER_TICKS = 4096;
     }
 
     public static class MotionProfiling {
-        
-    public static final double WHEEL_DIAMETER = 0.1524;
-    public static final double WHEEL_BASE_WIDTH = 0.6;
-    public static final double TIMEFRAME = 0.02;
 
-    public static final double MAX_VELOCITY = 3;
-    public static final double MAX_ACCELERATION = 2;
-    public static final double MAX_JERK = 80;
+        public static final double WHEEL_DIAMETER = 0.1524;
+        public static final double WHEEL_BASE_WIDTH = 0.6;
+        public static final double TIMEFRAME = 0.02;
 
-    public static final PidSettings MOTION_PROFILING_PID_SETTINGS_LEFT = new PidSettings(0.0, 0, 0.0, 0.0);//0.2103, 0.5139
-    public static final PidSettings MOTION_PROFILING_PID_SETTINGS_RIGHT = new PidSettings(0.0, 0, 0.0, 0.0);//0.1929, 2.5147
+        public static final double MAX_VELOCITY = 3;
+        public static final double MAX_ACCELERATION = 2;
+        public static final double MAX_JERK = 80;
 
-    public static final double MOTION_PROFILING_KP_TURN = 0.0025;
+        public static final PidSettings MOTION_PROFILING_PID_SETTINGS_LEFT = new PidSettings(0.0, 0, 0.0, 0.0); // 0.2103, 0.5139
+        public static final PidSettings MOTION_PROFILING_PID_SETTINGS_RIGHT = new PidSettings(0.0, 0, 0.0, 0.0); // 0.1929, 2.5147
 
-    public static final double TICKS_PER_METER_RIGHT = 575.0;
-    public static final double TICKS_PER_METER_LEFT = 771.5;
-    public static final int TICKS_PER_REVOLUTION_RIGHT = 290;
-    public static final int TICKS_PER_REVOLUTION_LEFT = 360;
-    }
-    
-    public static class Vision{
-        public static double DISTANCE_FROM_TARGET = 0;
-        public static double ANGLE_FROM_TARGET = 0;
+        public static final double MOTION_PROFILING_KP_TURN = 0.0025;
+
+        public static final double TICKS_PER_METER_RIGHT = 575.0;
+        public static final double TICKS_PER_METER_LEFT = 771.5;
+        public static final int TICKS_PER_REVOLUTION_RIGHT = 290;
+        public static final int TICKS_PER_REVOLUTION_LEFT = 360;
     }
 
-    public class Calibration{
+    public static class Vision {
+        public static final double DISTANCE_FROM_TARGET = 0;
+        public static final double ANGLE_FROM_TARGET = 0;
+    }
+
+    public class Calibration {
         public static final double leftForwardKv = 0.2103;
         public static final double leftForwardVi = 0.3717;
         public static final double rightForwardKv = 0.1929;
