55bec5d0864b5c55d083a2e80014d49cfd24300c
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2019-12-08 18:34:53 +0200

    Revert "made stuff work"
    This reverts commit f707f6cd

--- src/main/java/frc/robot/commands/Commands.java
+++ src/main/java/frc/robot/commands/Commands.java
@@ -3,10 +3,8 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
-import frc.robot.Enums.Path;
 import frc.robot.Robot;
 import frc.robot.RobotComponents;
-
 /**
  * this class holds all of the instant commands.
  */
@@ -58,13 +56,5 @@ public class Commands {
     public static InstantCommand toggleDrive() {
         InstantCommand c = new InstantCommand(() -> DriveArcade.toggleDrive());
         return c;
-    }
-    public static InstantCommand rereadPathFiles() {
-        var command = new InstantCommand(() -> {
-            for (Path path : Path.values())
-                path.readFromFiles();
-        });
-        command.setRunWhenDisabled(true);
-        return command;
     }
 }
